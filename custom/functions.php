9c71b54eea62524eec0c8767e111f6c50cb39f7f
Chris Rault <chris@devignerforhire.com>
2015-11-16 12:25:42 +0200

    Removed all test code

--- custom/functions.php
+++ custom/functions.php
@@ -1,25 +1,3 @@
 <?php
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
-
-// Enqueue custom assets
-add_action( 'beans_uikit_enqueue_scripts', 'tbr_child_theme_modifications_enqueue_uikit_assets', 5 );
-
-function tbr_child_theme_modifications_enqueue_uikit_assets() {
-
-	beans_uikit_enqueue_components( array( /* contrast */ ) );
-	beans_uikit_enqueue_components( array( /* sticky */ ), 'add-ons' );
-
-}
-
-
-// Customize the child theme output
-add_action( 'wp', 'tbr_child_theme_modifications_modify_child_theme' );
-
-function tbr_child_theme_modifications_modify_child_theme() {
-
-	beans_remove_markup( 'beans_site' );
-	beans_add_attribute( 'beans_header', 'data-uk-sticky', 'top:0' );
-
-}
