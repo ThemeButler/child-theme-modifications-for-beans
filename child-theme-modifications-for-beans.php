2b004bc614b4edae1f45c3bde1847acfa833960e
Chris Rault <chris@devignerforhire.com>
2015-11-18 15:32:53 +0200

    Defined the plugin paths as constants

--- child-theme-modifications-for-beans.php
+++ child-theme-modifications-for-beans.php
@@ -6,7 +6,7 @@
  * Author: 		ThemeButler
  * Author URI: 	http://www.themebutler.com/
  * Credits:		Originally inspired by the Theme Customizations by WooThemes.
- * @package Child_Theme_Modifications_For_Beans
+ * @package 	Child_Theme_Modifications_For_Beans
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -16,8 +16,24 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 if ( !file_exists( get_template_directory() . '/lib/api/init.php' ) )
 	return;
 
+
 // Include Beans
 require_once( get_template_directory() . '/lib/api/init.php' );
+
+
+// Define constants
+add_action( 'ctm_init', 'ctm_constant' );
+
+function ctm_constant() {
+
+	// File
+	define( 'CTM_FILE', __FILE__ );
+
+	// Path
+	define( 'CTM_PATH', plugin_dir_path( CTM_FILE ) );
+	define( 'CTM_CUSTOM_PATH', CTM_PATH . 'custom/' );
+
+}
 
 
 /**
@@ -41,9 +57,12 @@ function ctm_init() {
  */
 function ctm_uikit() {
 
-	beans_uikit_enqueue_theme( 'uikit', plugins_url( '/custom/uikit/', __FILE__  ) );
-	beans_compiler_add_fragment( 'uikit', plugins_url( '/custom/custom.less', __FILE__ ), 'less' );
-	beans_compiler_add_fragment( 'uikit', plugins_url( '/custom/custom.css', __FILE__ ), 'css' );
-	beans_compiler_add_fragment( 'uikit', plugins_url( '/custom/custom.js', __FILE__ ), 'js' );
+	beans_uikit_enqueue_theme( 'uikit', CTM_CUSTOM_PATH . 'uikit/' );
+	beans_compiler_add_fragment( 'uikit', CTM_CUSTOM_PATH . 'custom.less', 'less' );
+	beans_compiler_add_fragment( 'uikit', CTM_CUSTOM_PATH . 'custom.css', 'less' );
+	beans_compiler_add_fragment( 'uikit', CTM_CUSTOM_PATH . 'custom.js', 'js' );
 
 }
+
+// Initiate the plugin
+do_action( 'ctm_init' );
